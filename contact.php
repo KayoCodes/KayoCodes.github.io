63bd35d4de6d466675fdf9f1e1e8bf9ba61b04a0
Keenan <keenan.ray@my.utsa.edu>
2024-01-15 19:13:14 -0600

    updated php

---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php
+
+if(!empty($_POST["send"])){
+    $name =$_POST["name"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$message = $_POST["message"];
+$toMessage = "juskidn2@yahoo.com";
+
+$mailHeaders = "Name: " . $name . 
+"\r\n Email: " . $email .
+"\r\n Phone: " . $phone .
+"\r\n Message: " . $message . "\r\n";
+
+if(mail($toMessage,$name,$mailHeaders)){
+$message="Your Information is Received Successfully.";
+}
+}
+header('Location: index.html');
+
+?>
